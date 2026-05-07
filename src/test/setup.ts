4bcbeae2c2de192b7d52575cfca18a08ff70faaa
diguifi <lumia-diguifi@hotmail.com>
2026-05-07 18:39:02 -0300

    Fix assertions

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,7 +1,12 @@
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
-import { afterEach } from 'vitest';
+import { afterEach, beforeEach, vi } from 'vitest';
+
+beforeEach(() => {
+  vi.stubGlobal('open', vi.fn());
+});
 
 afterEach(() => {
   cleanup();
+  vi.unstubAllGlobals();
 });
